68e0bdc5b717327933524892833be2cef94a3f5f
eva-may-JS <xv4w04bu@students.codeinstitute.net>
2025-04-10 17:53:20 +0100

    Refactor functions, assign meaningful variable names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,21 @@
-function f1() {
-    var p = new Date();
-    var q = new Date("2025-12-25");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element");
-    variable2.innerText = variable;
-  }
-  
-  function f2() {
-    var p = new Date();
-    var q = new Date("2026-01-01");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element2");
-    variable2.innerText = variable;
-  }
-  
-  f1();
-  f2();
-  
+var today = new Date(); // Get the current date
+var christmas = new Date("2025-12-25"); // Set the date for Christmas
+var newYear = new Date("2026-01-01"); // Set the date for New Year
+
+function dateDiff(date1, date2) {
+  // Calculates the difference between two dates and returns the result in days.
+  var numDay = Math.round((date2 - date1) / (1000 * 60 * 60 * 24));
+  return numDay;
+}
+
+function displayDateDiff(elementId, date1, date2) {
+  // Takes the result of the previous function and displays it in the HTML element with id "my-element"
+  // and "my-element2" respectively.
+  var daysRemaining = dateDiff(date1, date2);
+  var htmlElement = document.getElementById(elementId);
+  htmlElement.innerText = daysRemaining;
+}
+
+// Display the countdown for Christmas and New Year
+displayDateDiff("my-element", today, christmas);
+displayDateDiff("my-element2", today, newYear);
