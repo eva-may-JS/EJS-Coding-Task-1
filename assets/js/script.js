36226f70bb4bd846b2d55ab1f6dfebf3161b76d5
eva-may-JS <xv4w04bu@students.codeinstitute.net>
2025-04-10 18:03:11 +0100

    Change function DateDiff name to calculateDateDiff

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var today = new Date(); // Get the current date
 var christmas = new Date("2025-12-25"); // Set the date for Christmas
 var newYear = new Date("2026-01-01"); // Set the date for New Year
 
-function dateDiff(date1, date2) {
+function calculateDateDiff(date1, date2) {
   // Calculates the difference between two dates and returns the result in days.
   var numDay = Math.round((date2 - date1) / (1000 * 60 * 60 * 24));
   return numDay;
@@ -11,7 +11,7 @@ function dateDiff(date1, date2) {
 function displayDateDiff(elementId, date1, date2) {
   // Takes the result of the previous function and displays it in the HTML element with id "my-element"
   // and "my-element2" respectively.
-  var daysRemaining = dateDiff(date1, date2);
+  var daysRemaining = calculateDateDiff(date1, date2);
   var htmlElement = document.getElementById(elementId);
   htmlElement.innerText = daysRemaining;
 }
