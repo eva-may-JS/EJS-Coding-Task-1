f72803c325627f1a105cc4957e180aa640a7ff5e
eva-may-JS <xv4w04bu@students.codeinstitute.net>
2025-04-11 21:59:16 +0100

    Improved solution to negative day issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 const currentYear = new Date().getFullYear(); // Current year
 
+function newDate(dateString) {
+  // Convert the input string to a Date object
+  const date = new Date(dateString);
+  const now = new Date();
+
+  // If the date has already passed this year, bump it to next year
+  if (date < now) {
+    return new Date(currentYear + 1, date.getMonth(), date.getDate());
+  } else {
+    return date;
+  }
+}
+
 const dates = [
   new Date(),
-  new Date(`${currentYear}-12-25`),
-  new Date(`${currentYear}-01-01`),
-  new Date(`${currentYear}-02-14`),
+  newDate(`${currentYear}-12-25`),
+  newDate(`${currentYear}-01-01`),
+  newDate(`${currentYear}-02-14`),
 ]; // Array of dates to be used in the countdown
 
 function calculateDateDiff(date1, date2) {
@@ -17,9 +30,9 @@ function displayDateDiff(elementId, date1, date2) {
   // Takes the result of the previous function and displays it in the HTML elements with corresponding id's
   var daysRemaining = calculateDateDiff(date1, date2);
   var htmlElement = document.getElementById(elementId);
-  if (daysRemaining < 0) {
-    daysRemaining = daysRemaining + 365; // If the difference is negative, add 365 days to it
-  }
+//  if (daysRemaining < 0) {
+//    daysRemaining = daysRemaining + 365; // If the difference is negative, add 365 days to it
+//  }
   htmlElement.innerText = daysRemaining;
 }
 
@@ -27,3 +40,11 @@ function displayDateDiff(elementId, date1, date2) {
 for (let i = 1; i < dates.length; i++) {
   displayDateDiff(`my-element${i}`, dates[0], dates[i]);
 }
+
+
+// if (numDay < 0) {
+//   // //currentYear = currentYear + 1; // If the difference is negative, set the year to the next year
+//   // dates[i] = new Date(`${currentYear + 1}-${dates[i].getMonth()}-${dates[i].getDate()}`);
+//   // console.log(dates[i]);
+//   // // Update the date to the next year
+//   // };
