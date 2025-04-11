55e9240d10a2702c9d78b5abb7ee588487f7f791
eva-may-JS <xv4w04bu@students.codeinstitute.net>
2025-04-11 15:33:31 +0100

    Add HTML and JS for Valentines countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const dates = [new Date(), new Date("2025-12-25"), new Date("2026-01-01")]; // Array of dates to be used in the countdown
+const dates = [new Date(), new Date("2025-12-25"), new Date("2026-01-01"), new Date("2025-02-14")]; // Array of dates to be used in the countdown
 
 function calculateDateDiff(date1, date2) {
   // Calculates the difference between two dates and returns the result in days.
@@ -17,3 +17,4 @@ function displayDateDiff(elementId, date1, date2) {
 // Display the countdowns
 displayDateDiff("my-element", dates[0], dates[1]); // Christmas countdown
 displayDateDiff("my-element2", dates[0], dates[2]); // New Year countdown
+displayDateDiff("my-element3", dates[0], dates[3]); // Valentines Day countdown
